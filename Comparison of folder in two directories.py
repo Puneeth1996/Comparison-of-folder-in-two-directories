baf631aea019da3e50a2632cc0f4bf117021c415
Puneeth1996 <puneeth1996p@gmail.com>
2018-09-13 14:57:56 +0530

    Comparison of folder in two directories

---
+++ Comparison of folder in two directories.py
@@ -0,0 +1,40 @@
+# Compare file that are present in two diffrent directories
+import os
+import time
+
+
+dir1 = os.listdir("C:\Intel")
+dir1_len=len(dir1)
+dir1_path = os.listdir(os.path.realpath("C:\Intel"))
+
+
+
+
+dir2 = os.listdir("D:\Intel")
+dir2_len=len(dir2)
+dir2_path = os.listdir(os.path.realpath("D:\Intel"))
+
+
+
+print '\n'
+print time.strftime('%Y - %m - %d')
+
+
+
+for i in range(3):
+	for j in range(1):
+		if dir1[i]==dir2[j]:
+			print 'match foun----'
+			print '\n'
+			print dir2[j]
+			print '\n'
+			print dir1[i]
+        
+
+    
+        
+        
+        
+    
+    
+        
