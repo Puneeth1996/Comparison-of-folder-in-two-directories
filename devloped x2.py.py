04531687499f8d4ea3068af61d8b81b85e0068f0
Puneeth1996 <puneeth1996p@gmail.com>
2018-09-29 19:51:44 +0530

    SECOND upagrade with automatic list analysis of two path

--- devloped x2.py.py
+++ devloped x2.py.py
@@ -4,28 +4,43 @@ import time
 
 
 dir1 = os.listdir("I:\Studies")
+
 dir1_len=len(dir1)
-dir1_path = os.listdir(os.path.realpath("C:\Intel"))
+
+print dir1_len
+
+dir1_path = os.listdir(os.path.realpath("I:\Studies"))
+
+
+print "\n\n"
 
 
 dir2 = os.listdir("J:\Course PART 2")
+
 dir2_len=len(dir2)
-dir2_path = os.listdir(os.path.realpath("D:\Intel"))
+
+print dir1_len
+
+dir2_path = os.listdir(os.path.realpath("J:\Course PART 2"))
 
 
 
 print '\n'
-print time.strftime('%Y - %m - %d')
+print time.strftime('%Y - %B - %d')
 
 
 
-for i in range(3):
-	for j in range(1):
+for i in range(dir1_len):
+	for j in range(dir2_len):
 		if dir1[i]==dir2[j]:
 			print 'match foun----'
+
 			print '\n'
+			
 			print dir2[j]
+
 			print '\n'
+
 			print dir1[i]
         
 
